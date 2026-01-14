1e9a5d04729c3a3f40290d54f8d464ea9106bbfd
Alex Jeffers <alex@gluestudio.co.uk>
2026-01-14 12:48:25 +0000

    prime now works for scotland

--- src/components/select-county.jsx
+++ src/components/select-county.jsx
@@ -5,7 +5,8 @@ class SelectCounty extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            selectedValue: ''
+            selectedValue: '',
+            displayInfo: 'none'
         }
     }
 
@@ -14,6 +15,10 @@ class SelectCounty extends React.Component{
             selectedValue: e.target.value
         })
         this.props.handleSelectCounty(e.target.value)
+    }
+
+    handleInfoBoxClick = () => {
+        this.state.displayInfo === 'none' ? this.setState({displayInfo: 'block'}) : this.setState({displayInfo: 'none'})
     }
 
     render(){
@@ -28,6 +33,16 @@ class SelectCounty extends React.Component{
                         return <option key={index} value={county}>{county}</option>
                     })}
                 </select>
+                <div className='prime-only-container'>
+                <img alt="" onClick={this.handleInfoBoxClick} style={{width: "15px", marginRight: '10px'}}
+                     src="info.svg"/>
+                <p className="info-window" style={{display: this.state.displayInfo}}>A prime location is defined as an area where property values are at least 20% higher than the regional average. Whilst all locations have prime areas, not all have them listed, as small villages or hamlets may not qualify under the research criteria.</p>
+                <label
+                    style={{whiteSpace: 'nowrap'}} htmlFor="primeOnly"> Prime Locations Only </label>
+                <input style={{margin: 0}} type="checkbox" id="primeOnly" name="primeOnly"
+                       checked={this.props.primeOnly}
+                       onChange={(e) => this.props.handlePrimeOnly(e.target.checked)}/>
+            </div>
             </div>
         )
     }
